0070693a8aa805d360477113d60ba3a858edf8e7
daiyeyue <150064328@qq.com>
2019-01-28 15:06:52 +0800

    gender

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,5 +9,15 @@ age = 19
 if age < 18:
     print("小于18岁");
 else:
-    print("大于19岁")
+    print("大于19岁");
 
+gender = input("请输入性别：")
+print("你输入的性别是：{0}".format(gender))
+
+if gender == "nan":
+    print("来，我们纪念一下今天吧，代码敲十遍")
+else:
+    print("发糖喽发糖喽")
+    print("你是女生，特殊照顾喽")
+
+print("开始上课喽")
